c0b293c8ad956385e1e41598adc9082c48c09507
perlinwarp <perlinwarp@gmail.com>
2018-10-09 10:44:35 +0100

    Started making docs

--- MiniProject1/leds.c
+++ MiniProject1/leds.c
@@ -7,6 +7,7 @@
 int timems = 0;
 // Function Prototypes
 void delayms(int);
+void stage1();
 
 // Define our pin numbers
 #define LED1 18
@@ -17,22 +18,38 @@ int LEDs[4] = {1<<LED1, 1<<LED2, 1<<LED3, 1<<LED4};
 int allLEDs= (1<<LED1)|(1<<LED2)|(1<<LED3)|(1<<LED4);
 
 int main(){
+  // Initialize SysTick interrupts every ms
+  SysTick_Config(SystemCoreClock / 1000);
+
   // Initialize LED GPIO to out
   GPIO_SetDir(1, 0xFFFFFFFF, 1);
   int digit = 2;
   GPIO_SetValue(1, LEDs[digit]);
   GPIO_SetValue(1, LEDs[1]);
 
-  while (true){
-    delayms(1000);
+  while (1==1){
     GPIO_SetValue(1, allLEDs);
     delayms(1000);
-    GPIO_SetValue(1, allLEDs);
+    GPIO_SetValue(0, allLEDs);
+    delayms(1000);
   }
 
+}
+
+void SysTick_Handler(void){
+	timems++;
 }
 
 void delayms(int ms){
 	int start = timems;
 	while(timems - start < ms){}
+}
+
+void stage1(){
+  int i;
+  for(i = 0; i < 3; i++){
+    GPIO_SetValue(0, allLEDs);
+    GPIO_SetValue(1, LEDs[i]);
+    delayms(1000);
+  }
 }
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # EMPR
+
+## Note when using Serial on uni computers. You need to use xfce terminal not gnome terminal.
+
+##Delays
+
+##LEDs
+### LED Pin numbers
+* LED1 18
+* LED2 20
+* LED3 21
+* LED4 23
